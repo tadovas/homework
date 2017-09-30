b98878eaec58f66b51800a1e861c6df1df5ffdcb
tadovas <tadovas@gmail.com>
2017-09-30 17:19:41 +0300

    Skeleton

---
+++ src/main/java/org/solar/crawlerlog/CrawlerLogApplication.java
@@ -0,0 +1,12 @@
+package org.solar.crawlerlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerLogApplication.class, args);
+	}
+}
