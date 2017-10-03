b359aaa9e91faef038bc725b89ef2fa6b835b06b
tadovas <tadovas@gmail.com>
2017-10-03 21:13:03 +0300

    Dropwizard style metrics added

--- src/main/java/org/solar/crawlerlog/domain/service/CrawlerLogService.java
+++ src/main/java/org/solar/crawlerlog/domain/service/CrawlerLogService.java
@@ -1,6 +1,9 @@
 package org.solar.crawlerlog.domain.service;
 
 
+import com.codahale.metrics.annotation.Counted;
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
 import org.solar.crawlerlog.domain.model.*;
 import org.solar.crawlerlog.domain.repository.CrawlerLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +23,8 @@ public class CrawlerLogService {
         this.repository = crawlerLogRepository;
     }
 
-
+    @Timed
+    @ExceptionMetered
     public CreationResult createNewCrawlerLog(SourceUrl sourceUrl) {
 
         LogId newLogId = repository.nextLogId();
@@ -32,16 +36,25 @@ public class CrawlerLogService {
         return CreationResult.newResult( newLogId , anyUnfinishedLogsExist);
     }
 
+    @Timed
+    @ExceptionMetered
     public void addCelebrities(LogId id , Collection<Celebrity> celebrities) {
 
         findApplyAndSave(id , crawlerLog -> crawlerLog.addCelebrities(celebrities));
     }
 
+    @Timed
+    @ExceptionMetered
     public void finishCrawlerLog(LogId id, RemoteRepositoryId repositoryId) {
 
         findApplyAndSave(id , crawlerLog -> crawlerLog.finish(repositoryId));
     }
 
+    @Timed
+    @ExceptionMetered
+    public CrawlerLog findCrawlerLogById(LogId id) {
+        return findOrThrowException(id);
+    }
 
     private CrawlerLog findOrThrowException(LogId id) {
         return repository.findById(id).orElseThrow(() -> new CrawlerLogNotFoundException("Log not found with id: " + id));
@@ -52,10 +65,6 @@ public class CrawlerLogService {
         CrawlerLog log = findOrThrowException(id);
         crawlerLogConsumer.accept(log);
         repository.save(log);
-    }
-
-    public CrawlerLog findCrawlerLogById(LogId id) {
-        return findOrThrowException(id);
     }
 }
 
---
+++ src/main/java/org/solar/crawlerlog/infrastructure/MetricsConfiguration.java
@@ -0,0 +1,9 @@
+package org.solar.crawlerlog.infrastructure;
+
+import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableMetrics
+public class MetricsConfiguration {
+}
