6795f6303578750f5f21ba90cc5adef7c514946a
tadovas <tadovas@gmail.com>
2017-10-04 23:06:12 +0300

    Even more formatting, tabs replaced by spaces

--- crawler-log-service/src/main/java/org/solar/crawlerlog/web/docs/SwaggerConfiguration.java
+++ crawler-log-service/src/main/java/org/solar/crawlerlog/web/docs/SwaggerConfiguration.java
@@ -5,13 +5,13 @@ package org.solar.crawlerlog.web.docs;
 Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths((not(or(ant("/error") , ant("/")))))
-				.build();
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths((not(or(ant("/error") , ant("/")))))
+                .build();
+    }
 }
 */
